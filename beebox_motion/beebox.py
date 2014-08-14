37d2eb6a7ae9b41d808bacaf854e48a528507d55
Gabriel Duarte <confusosk8@gmail.com>
2014-08-14 16:44:35 -0300

    initial commint

---
+++ beebox_motion/beebox.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python
+#-*- coding:utf-8 -*-#
+
+import sys
+import signal
+from optparse import OptionParser
+from OSC import OSCClient
+from beemotion import MotionDetec
+from arduinoserial import ArduinoSerial
+
+def signal_handler(signal, frame):
+        print 'You killed me!'
+        sys.exit(0)
+
+
+if __name__=="__main__":
+    parser = OptionParser(usage="usage: %prog [options]",
+                          version="%prog 0.1")
+    parser.disable_interspersed_args()
+    parser.add_option('--sport', '-p', dest='port', type="string", help="Select the serial port to be used")
+    parser.add_option('--osc', '-s', dest='server', type="string", help="Point to OSC server")
+    parser.add_option('--oport', '-o', dest='oscport', type="int", help="Server Port")
+
+    
+    (opts, remainder) = parser.parse_args()
+
+    if opts.server is None:
+        print 'You must point the server where data will be sent!'
+        sys.exit(-1)
+
+    
+    if opts.oscport is None:
+        print 'You must tell the port used on OSC server'
+        sys.exit(-1)
+
+
+    if opts.port is None:
+        print "Serial port not chosen"
+        sys.exit(-1)
+
+
+
+    client = OSCClient()
+    client.connect( (opts.server, opts.oscport) )
+
+    #client.send( OSCMessage("/gduarte/1", [1.0, 2.0, 3.0 ] ) )
+    #client.send( OSCMessage("/gduarte/2", [2.0, 3.0, 4.0 ] ) )
+    #client.send( OSCMessage("/gduarte/3", [2.0, 3.0, 3.1 ] ) )
+    #client.send( OSCMessage("/gduarte/4", [3.2, 3.4, 6.0 ] ) )
+    #client.send( OSCMessage("/quit") )
+    threads = []
+    
+    motion = MotionDetec(client)
+    serial = ArduinoSerial(client, opts.port)
+
+    threads.append(motion)
+    threads.append(serial)
+    serial.start()
+    motion.start()
+
+    while len(threads) > 0:
+        try:
+            # Join all threads using a timeout so it doesn't block
+            # Filter out threads which have been joined or are None
+            threads = [t.join(1) for t in threads if t is not None and t.isAlive()]
+        except KeyboardInterrupt:
+            print "Ctrl-c received! Sending kill to threads..."
+            for t in threads:
+                t.kill_received = True
+
+
+    
